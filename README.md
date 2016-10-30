df801d8ca7b8b321dc631f1c7daad5c10ae13b0e
Sujay Mansingh <sujaymansingh@gmail.com>
2016-10-30 23:41:13 +0000

    Convert into python package

---
+++ README.md
@@ -0,0 +1,8 @@
+## Countdown Slackbot!
+
+A bot that handles countdown games.
+
+```
+$ export SLACKBOT_API_TOKEN=...
+$ python -m countdown_slackbot
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import sys
+
+
+REQUIREMENTS = [
+    "nose>=1.3,<1.4",
+    "slackbot>=0.4,<0.5"
+]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "requirements":
+        for req in REQUIREMENTS:
+            print(req)
+        sys.exit(0)
+
+    setuptools.setup(
+        name="countdown-slackbot",
+        version="0.0.1",
+        author="Sujay Mansingh",
+        author_email="sujay.mansingh@gmail.com",
+        packages=setuptools.find_packages(),
+        scripts=[],
+        url="https://github.com/sujaymansingh/countdown-slackbot",
+        license="LICENSE.txt",
+        description="A slackbot that plays countdown",
+        long_description="View the url for more details.",
+        install_requires=REQUIREMENTS
+    )
